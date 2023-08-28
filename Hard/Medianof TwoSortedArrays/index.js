b57faea4d84abf7b9b1620706654494e6871a041
sana <sana.fathi30@gmail.com>
2023-08-28 22:00:44 +0330

    clean some log

--- Hard/Medianof TwoSortedArrays/index.js
+++ Hard/Medianof TwoSortedArrays/index.js
@@ -3,22 +3,18 @@ function sum(arr1, arr2) {
   numbers = numbers.sort(function (a, b) {
     return a - b;
   });
-  console.log(numbers);
   if (numbers.length % 2 === 0) {
     let m = numbers.length / 2;
     let k = (numbers[m] + numbers[m - 1]) / 2;
     k = k.toFixed(5);
-    console.log(k);
     return k;
   } else {
     if (numbers.length === 1) {
-      console.log(numbers[numbers.length - 1]);
       return numbers[numbers.length - 1];
     } else {
       let length = numbers.length / 2;
       length = Math.round(length);
       length = length.toFixed(5);
-      console.log(numbers[length - 1]);
       return numbers[length - 1];
     }
   }
